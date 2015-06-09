ce5d749a3c08203d0cf58d2453e2eddc8451c1a0
Taskmaster <TheEnd@Taskmasters-iMac.local>
2015-06-08 23:06:46 -0400

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -60,7 +60,7 @@ static const int GRID_COLUMNS = 10;
             _gridArray[i][j] = creature;
             
             // Make creatures visible to test this method, remove this once we know we have filled the grid properly.
-            creature.isAlive = YES;
+            // creature.isAlive = YES;
             
             x += _cellWidth;
         }
